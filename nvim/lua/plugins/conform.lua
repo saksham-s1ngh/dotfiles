fcf466c34337e425c37a9a79e9f29e7b5720d392
saksham-s1ngh <saksham249singh@gmail.com>
2026-09-17 12:21:22 -0400

    removed isort + black for ruff Was seeing timeout errors in neovim with black and isort. Removed both of them in favour of ruff, given that ruff is faster (as found when searching with Gemini and Claude).

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,36 +1,36 @@
 -- Mason tools to install manually via :Mason
--- Formatters: stylua, prettier, isort, black, csharpier
+-- Formatters: stylua, prettier, ruff, csharpier
 -- Built into language toolchain (no Mason needed): rustfmt (Rust)
 
 return {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
-  config = function()
-    require("conform").setup({
-      formatters_by_ft = {
-        lua                = { "stylua" },
-        python             = { "isort", "black" },
-        javascript         = { "prettier" },
-        javascriptreact    = { "prettier" },
-        typescript         = { "prettier" },
-        typescriptreact    = { "prettier" },
-        html               = { "prettier" },
-        css                = { "prettier" },
-        json               = { "prettier" },
-        yaml               = { "prettier" },
-        markdown           = { "prettier" },
-        rust               = { "rustfmt" },
-        cs                 = { "csharpier" },
-      },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_format = "fallback",
-      },
-    })
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "ruff_organize_imports", "ruff_format" },
+				javascript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescript = { "prettier" },
+				typescriptreact = { "prettier" },
+				html = { "prettier" },
+				css = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				rust = { "rustfmt" },
+				cs = { "csharpier" },
+			},
+			format_on_save = {
+				timeout_ms = 2000,
+				lsp_format = "fallback",
+			},
+		})
 
-    vim.keymap.set({ "n", "v" }, "<leader>gf", function()
-      require("conform").format({ async = true })
-    end, { desc = "Format buffer" })
-  end,
+		vim.keymap.set({ "n", "v" }, "<leader>gf", function()
+			require("conform").format({ async = true })
+		end, { desc = "Format buffer" })
+	end,
 }
